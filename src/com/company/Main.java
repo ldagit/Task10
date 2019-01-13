d51bd2a29c4fd5d84f877ccc41be8d560b8f3816
ldagit <litvintsova@gmail.com>
2019-01-13 18:32:54 +0300

    Ввод строки с клавиатуры

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+// write your code here
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите строку: ");
+        String Str = in.nextLine();
+        System.out.println(Str);
+    }
+}
